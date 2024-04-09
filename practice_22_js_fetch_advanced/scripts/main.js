8e5292e90ff3837092d367c09d3cbc4be97a84b0
Davy <davyd.dabahian@cheetahdigital.com>
2024-04-09 23:22:09 +0300

    practice 23 sass

--- practice_22_js_fetch_advanced/scripts/main.js
+++ practice_22_js_fetch_advanced/scripts/main.js
@@ -13,7 +13,36 @@ const task1Demo = () => {
       }, 3000);
   });
   // Здесь нужно дождаться выполнения обеих промисов и вывести сумму результатов в консоль
+  Promise.all( [firstPromise, secondPromise] )
+    .then( data => console.log('Result', data[0] + data[1]));
+  /*
+  1. У нас есть функция `task1Demo`. В ней есть 2 промиса, каждый из которых возвращает "часть от целого". Нужно дождаться выполнения обеих промисов, и вывести в консоль сумму их результатов.
+  */
 }
+
+// task1Demo();
+
+/*
+2. Освежаем в памяти нештатные ситуации. Пишем функцию `randomPromise`. Она должна возвращать промис, который будет завершен успехом или неуспехом, в зависимости от случайного значения. То есть, в функции нужно взять случайное значение (`Math.random()`), и если его значение меньше 0.5 - завершать возвращаемый промис успехом, а иначе - завершать неуспехом.
+*/
+function randomPromise() {
+    const randomValue = Math.random();
+    return new Promise((resolve, reject) => {
+        if (randomValue < 0.5) {
+            resolve(5);
+        }
+        else {
+            reject('Something went wrong!');
+        }
+    });
+}
+
+/*
+3. Работаем с нештатной ситуацией. У нас есть функция `task3Demo`, в ней также 2 промиса, но второй промис может быть завершен с ошибкой. Нужно построить такую логику, чтобы:
+ - при успешном завершении обоих промисов, в консоли выводилась сумма результатов (как в предыдущей задаче)
+ - в случае ошибки, в консоль выводилось "something went wrong!", и причина ошибки (то, что передается в `reject`)
+ - вне зависимости от результата, чтобы в консоль выводилось "All promises are finished now!".
+ */
 
 
 
@@ -39,7 +68,21 @@ const task3Demo = () => {
       }, 3000);
   });
   // Здесь нужно добавить логику в соответствии с постановкой задачи.
+  Promise.all( [firstPromise, secondPromise] )
+    .then( data => {
+        console.log(data[0] + data[1]);
+    })
+    .catch( rejected => {
+        console.log('something went wrong', rejected);
+    })
+    .finally( () => {
+        console.log('All promises are finished now!')
+    } );
 }
+
+// task3Demo();
+
+
 
 // Для задачи 4
 const fastAndFurious = () => {
@@ -53,9 +96,46 @@ const fastAndFurious = () => {
     const rand = Math.random() * 1000;
     results.push( new Promise( resolve => {
       setTimeout( () => {
-        resolve(`${ptr.name} on ${ptr.car}`);
+        resolve(`${ptr.name} on ${ptr.car}. Time: ${rand}`);
       }, rand );
     }));
    }
    return results;
 }
+
+Promise.race( fastAndFurious() )
+    .then( data => {
+        console.log(data)
+    });
+
+
+/*
+5. Пишем функцию добавления нового поста `addPost`. В аргументах она принимает:
+ - title - заголовок поста
+ - body - текст поста
+ - userId - ID пользователя, создавшего пост
+
+Она должна обратиться POST-запросом к адресу `https://jsonplaceholder.typicode.com/posts`, и в теле запроса передать объект со свойствами из аргументов. Объект нужно преобразовать в строку (`JSON.stringify`).
+*/
+function addPost(title, body, userId) {
+    const payload = JSON.stringify({ title, body, userId });
+    fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            body: payload
+        }
+    )
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                throw new Error(response.statusText);
+            }
+        })
+        .then( data => console.log(data))
+        .catch( error => console.log(error));
+}
+
+addPost('Title', 'Body', 1)
