9fdd155b72053bc06fdbfeec2769a1b911431530
Davy <davyd.dabahian@cheetahdigital.com>
2024-04-22 09:39:40 +0300

    practice 26 OOP static, getters, inheritance

---
+++ practice_26_OO_private_get_set/scripts/main.js
@@ -0,0 +1,9 @@
+class Product {
+
+  constructor(title, category, price, description) {
+    this.title = title;
+    this.category = category;
+    this.price = price;
+    this.description = description;
+  }
+}
